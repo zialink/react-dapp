6dc3c74f790d81eb1b48708863db80dba029f6f8
zia <zedlinkus@gmail.com>
2022-03-07 00:52:19 +0100

    Made state changes sync with UI display

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,20 +6,22 @@
 const hre = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const [deployer] = await hre.ethers.getSigners();
+
+  console.log("Deploying contracts with the acount: ", deployer.address);
 
   // We get the contract to deploy
   const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const greeter = await Greeter.deploy("Hello, Zia!");
+
+  const Token = await hre.ethers.getContractFactory("Token");
+  const token = await Token.deploy();
 
   await greeter.deployed();
+  await token.deployed();
 
   console.log("Greeter deployed to:", greeter.address);
+  console.log("Token deployed to:", token.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- src/App.js
+++ src/App.js
@@ -1,39 +1,53 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ethers } from "ethers";
 import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json";
+import Token from "./artifacts/contracts/Token.sol/Token.json";
 
-const greeterAddress = "0x52fe55aF9AB97C68dE5CeE87FE140C9D8d53B012";
+const greeterAddress = "0xBB2816b38C63668a3f7B2c0Afc084320C9853e11";
+const tokenAddress = "0x34a98b7A7ea4bDA2F2913809778b9459DCfb399a";
 
 function App() {
   const [greeting, setGreetingValue] = useState(null);
-  const [greetingMessage, setGreetingMessage] = useState("Hi");
+  const [greetingMessage, setGreetingMessage] = useState();
+  const [userAccount, setUserAccount] = useState();
+  const [amount, setAmount] = useState(0);
+  const [balance, setBalance] = useState();
+  const [successfulTransaction, setSuccessfulTransaction] = useState(false);
 
   //request access to the user's MetaMask account
   async function requestAccount() {
-    await window.ethereum.request({ method: "eth_requestAccounts" });
+    const [account] = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    return account;
   }
 
   // call the smart contrat, read the current greeting value
-  async function fetchGreeting() {
-    if (typeof window.ethereum !== "undefined") {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const contract = new ethers.Contract(
-        greeterAddress,
-        Greeter.abi,
-        provider
-      );
-      try {
-        const data = await contract.greet();
-        console.log("data: ", data);
-        setGreetingMessage(data);
-      } catch (error) {
-        console.log("Error: ", error);
+  useEffect(() => {
+    async function fetchGreeting() {
+      if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const contract = new ethers.Contract(
+          greeterAddress,
+          Greeter.abi,
+          provider
+        );
+        try {
+          const data = await contract.greet();
+          console.log("data: ", data);
+          setGreetingMessage(data);
+        } catch (error) {
+          console.log("Error: ", error);
+        }
       }
     }
-  }
+
+    fetchGreeting();
+  }, [greetingMessage, successfulTransaction]);
 
   // call the smart contract, send an update
   async function setGreeting() {
+    setSuccessfulTransaction(false);
     if (!greeting) return;
     if (typeof window.ethereum !== "undefined") {
       await requestAccount();
@@ -42,22 +56,67 @@ function App() {
       const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
       const transaction = await contract.setGreeting(greeting);
       await transaction.wait();
-      fetchGreeting();
+      transaction && setSuccessfulTransaction(true);
     }
   }
+
+  useEffect(() => {
+    async function getBalance() {
+      if (typeof window.ethereum !== "undefined") {
+        const account = requestAccount();
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const contract = new ethers.Contract(tokenAddress, Token.abi, provider);
+        const _getBalance = await contract.balanceOf(account);
+        setBalance(_getBalance.toString());
+      }
+    }
+
+    getBalance();
+  }, [successfulTransaction, balance]);
+
+  async function sendCoins() {
+    setSuccessfulTransaction(false);
+    if (typeof window.ethereum !== "undefined") {
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(tokenAddress, Token.abi, signer);
+      const transaction = await contract.transfer(userAccount, amount);
+      await transaction.wait();
+      transaction && setSuccessfulTransaction(true);
+    }
+  }
+
   return (
     <div>
       <header>
         <div>{greetingMessage && greetingMessage}</div>
-        <div>
-          <button onClick={fetchGreeting}> Fetch Greeting</button>
-        </div>
+        {successfulTransaction && balance ? (
+          <div>
+            You have successfully transeferred {amount} of ZETH, your balance is{" "}
+            {balance}
+          </div>
+        ) : (
+          balance && <div> Your wallet balance is {balance} </div>
+        )}
+
         <div>
           <input
             onChange={(e) => setGreetingValue(e.target.value)}
             placeholder="Set greeting"
           />
           <button onClick={setGreeting}>Set Greeting</button>
+        </div>
+        <div>
+          <input
+            onChange={(e) => setUserAccount(e.target.value)}
+            placeholder="Account ID"
+          />
+          <input
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder="Amount"
+          />
+          <button onClick={sendCoins}>Send Coins</button>
         </div>
       </header>
     </div>
